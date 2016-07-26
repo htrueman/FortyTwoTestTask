ece7651202edb3fc6fd9ade1c90219d57fea85c2
htrueman <vege1wgw@gmail.com>
2016-07-26 16:55:48 +0300

    t1_p2

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -8,7 +8,6 @@ urlpatterns = patterns(
     # Examples:
     # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'hello.views.contact_data', name='contacts'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
