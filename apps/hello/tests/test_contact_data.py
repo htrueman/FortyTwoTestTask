b94602a998222d6f15383c7b4bc2e984ffffcfac
htrueman <vege1wgw@gmail.com>
2016-07-27 16:47:27 +0300

    t1_fin2 tests_upd

--- apps/hello/tests/test_contact_data.py
+++ apps/hello/tests/test_contact_data.py
@@ -5,67 +5,68 @@ from apps.hello.models import MyData
 
 
 class TestContctData(TestCase):
+
 	def setUp(self):
-        MyData.objects.all().delete()
-        MyData.objects.get_or_create(
+		MyData.objects.all().delete()
+		MyData.objects.get_or_create(
             name='Name1',
             last_name='LastName1',
             birthday='2001-01-01',
             bio='Bio1',
             email='Email@email1',
-            jabber='J@jabber1'
+            jabber='J@jabber1',
             skype='Skype1',
             other_conts='Conts1'
         )
 
-    def test_one(self):
+	def test_one(self):
         # test if one odject in database
-        self.client = Client()
-        self.url = reverse('contacts')
-        response = self.client.get(self.url)
+		self.client = Client()
+		self.url = reverse('contacts')
+		response = self.client.get(self.url)
 
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('Name1', response.content)
-        self.assertIn('LastName1', response.content)
-        self.assertIn('Bio1', response.content)
-        self.assertIn('Email@email1', response.content)
-        self.assertIn('J@jabber1', response.content)
-        self.assertIn('Skype1', response.content)
-        self.assertIn('Conts1', response.content)
+		self.assertEqual(response.status_code, 200)
+		self.assertIn('Name1', response.content)
+		self.assertIn('LastName1', response.content)
+		self.assertIn('Bio1', response.content)
+		self.assertIn('Email@email1', response.content)
+		self.assertIn('J@jabber1', response.content)
+		self.assertIn('Skype1', response.content)
+		self.assertIn('Conts1', response.content)
 
-    def test_many(self):
+	def test_many(self):
         # test if more than one odject in database
-        AboutMe.objects.all().delete()
-        AboutMe.objects.get_or_create(
-            name='Name2',
-            last_name='LastName2',
-            birthday='2002-02-02',
-            bio='Bio2',
-            email='Email@email2',
-            jabber='J@jabber2'
-            skype='Skype2',
-            other_conts='Conts2'
+		MyData.objects.all().delete()
+		MyData.objects.get_or_create(
+			name='Name2',
+ 			last_name='LastName2',
+			birthday='2002-02-02',
+			bio='Bio2',
+			email='Email@email2',
+			jabber='J@jabber2',
+			skype='Skype2',
+			other_conts='Conts2'
         )
-        AboutMe.objects.get_or_create(
-            name='Name3',
-            last_name='LastName3',
-            birthday='2003-03-03',
-            bio='Bio3',
-            email='Email@email3',
-            jabber='J@jabber3'
-            skype='Skype3',
-            other_conts='Conts3'
+		MyData.objects.get_or_create(
+			name='Name3',
+			last_name='LastName3',
+			birthday='2003-03-03',
+			bio='Bio3',
+			email='Email@email3',
+			jabber='J@jabber3',
+			skype='Skype3',
+			other_conts='Conts3'
         )
 
-        self.client = Client()
-        self.url = reverse('contacts')
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
+		self.client = Client()
+		self.url = reverse('contacts')
+		response = self.client.get(self.url)
+		self.assertEqual(response.status_code, 200)
 
-        self.assertIn('Name2', response.content)
-        self.assertIn('LastName2', response.content)
-        self.assertIn('Bio2', response.content)
-        self.assertIn('Email@email2', response.content)
-        self.assertIn('J@jabber2', response.content)
-        self.assertIn('Skype2', response.content)
-        self.assertIn('Conts2', response.content)
+		self.assertIn('Name2', response.content)
+		self.assertIn('LastName2', response.content)
+		self.assertIn('Bio2', response.content)
+		self.assertIn('Email@email2', response.content)
+		self.assertIn('J@jabber2', response.content)
+		self.assertIn('Skype2', response.content)
+		self.assertIn('Conts2', response.content)
--- apps/hello/tests/test_models.py
+++ apps/hello/tests/test_models.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 
 from apps.hello.models import MyData
 
+
 class MyDataModelTests(TestCase):
     def test_str(self):
         data = MyData(name='Name')
-        self.assertEqual(str(info), u'Name')
+        self.assertEqual(str(data), u'Name ')
