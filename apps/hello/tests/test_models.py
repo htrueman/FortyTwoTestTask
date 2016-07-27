bb5933e5f47be8aa0ac668b385cb45783243cf29
htrueman <vege1wgw@gmail.com>
2016-07-27 16:59:03 +0300

    t1_fin2 space_int

--- apps/hello/tests/test_models.py
+++ apps/hello/tests/test_models.py
@@ -4,7 +4,7 @@ from apps.hello.models import MyData
 
 
 class MyDataModelTests(TestCase):
-	""" check value that return MyData """
-	def test_str(self):
-		data = MyData(name='Name')
-		self.assertEqual(str(data), u'Name ')
+    """ check value that return MyData """
+    def test_str(self):
+        data = MyData(name='Name')
+        self.assertEqual(str(data), u'Name ')
