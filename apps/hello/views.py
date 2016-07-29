e8f5d153cb6e08f05077f53507a28fdd5dd1fb56
htrueman <vege1wgw@gmail.com>
2016-07-30 01:01:40 +0300

    RequestKeeperModel added

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -24,16 +24,17 @@ def contact_data(request):
     else:
         return render(request, 'contacts.html', {})
 
+
 def requests_data(request):
     requests = [
-    'GET /requests/ 200',
-    'GET /edit/add/ 200',
-    'GET /img/upload/img1.png 304',
-    'GET /img/upload/img2.png 404',
-    'GET /requests/ 200',
-    'GET / 200',
-    'GET /requests/ 200',
-    'GET / 200',
-    'GET /requests/ 200',
-    'GET /edit/ 200']
+    	'GET /requests/ 200',
+    	'GET /edit/add/ 200',
+    	'GET /img/upload/img1.png 304',
+    	'GET /img/upload/img2.png 404',
+    	'GET /requests/ 200',
+    	'GET / 200',
+    	'GET /requests/ 200',
+    	'GET / 200',
+    	'GET /requests/ 200',
+    	'GET /edit/ 200']
     return render(request, 'requests.html', {'requests':requests[-10:]})
