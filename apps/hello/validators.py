f0988c579c7abe17db5c141673e3a877208cb45d
htrueman <vege1wgw@gmail.com>
2016-07-30 21:38:49 +0300

    validation fixed

--- apps/hello/validators.py
+++ apps/hello/validators.py
@@ -6,6 +6,9 @@ from django.core.exceptions import ValidationError
 def validate_birthday(value):
     if value > datetime.datetime.now().date():
         raise ValidationError("Please write your real date of birth!")
+    else:
+    	# added to fix flake8 caution
+    	return None
 
 # by the time when passing data to birthday field this field doesn't
 # contain any data and it can't be sent to the validator
