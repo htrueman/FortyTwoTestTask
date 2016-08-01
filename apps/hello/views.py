ea2a8ff46b8194bc96f369fa8cf7a3a9a3b8f112
htrueman <vege1wgw@gmail.com>
2016-08-01 20:32:57 +0300

    test_middleware

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -15,22 +15,6 @@ def contact_data(request):
     return render(request, 'contacts.html', {'data': data})
 
 
-#def requests_data(request, order='dafault'):
-
-#   requests = [
- #       'GET /requests/ 200',
-#       'GET /edit/add/ 200',
-  #      'GET /img/upload/img1.png 304',
-  #      'GET /img/upload/img2.png 404',
-  #      'GET /requests/ 200',
-   #     'GET / 200',
-  #      'GET /requests/ 200',
-  #      'GET / 200',
-  #      'GET /requests/ 200',
-  #      'GET /edit/ 200']
-  #  return render(request, 'requests.html', {'requests': requests[-10:]}) """
-
-
 class RequestKeeperView(ListView):
     model = RequestKeeperModel
     template_name = 'requests.html'
