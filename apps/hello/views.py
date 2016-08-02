f57a79314453c28716596cfaf8e1f1356a9c7936
htrueman <vege1wgw@gmail.com>
2016-08-02 23:53:50 +0300

    logging settings to settings module added

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -6,6 +6,5 @@ from apps.hello.models import MyData
 
 def contact_data(request):
     data = MyData.objects.first()
-    logging.basicConfig(filename='cont.log', level=logging.DEBUG)
     logging.debug(data)
     return render(request, 'contacts.html', {'data': data})
