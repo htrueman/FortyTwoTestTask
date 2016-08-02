6784d51b813a65787df8583ce9819f9040e77ec4
htrueman <vege1wgw@gmail.com>
2016-08-02 20:37:07 +0300

    t3 fin

--- apps/hello/tests/test_models.py
+++ apps/hello/tests/test_models.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.core.exceptions import ValidationError
 import traceback
 
-from apps.hello.models import MyData
+from apps.hello.models import MyData, RequestKeeperModel
 
 
 class MyDataModelTests(TestCase):
@@ -30,3 +30,16 @@ class MyDataModelTests(TestCase):
             test.clean_fields()
         except ValidationError:
             self.assertIn("ValidationError", traceback.format_exc())
+
+
+class RequestKeeperModelTests(TestCase):
+
+    def test_create_fields(self):
+        """ check if model's fields created successfully """
+        test_req = RequestKeeperModel.objects.create(
+                path='path',
+                method='method',
+                date=datetime.date(2016, 1, 1),
+                is_viewed=True
+            )
+        test_req.clean_fields()
