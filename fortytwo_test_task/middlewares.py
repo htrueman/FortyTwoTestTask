c3c3a832c716e333c21daf328c7f0cf64ff5cce0
htrueman <vege1wgw@gmail.com>
2016-08-09 15:30:14 +0300

    test middleware

--- fortytwo_test_task/middlewares.py
+++ fortytwo_test_task/middlewares.py
@@ -1,11 +1,21 @@
 from apps.hello.models import RequestKeeperModel
-
+from django.contrib.auth.models import User
 
 class RequestKeeperMiddleware():
     def process_request(self, request):
         is_utility_request = '/requests/fetching' in request.get_full_path()
         if not is_utility_request:
-            RequestKeeperModel.objects.create(
-                path=request.get_full_path(),
-                method=request.method
-            )
+            if request.user.is_authenticated():
+                # set current username into author's field if user is authenticated 
+                RequestKeeperModel.objects.create(
+                    path=request.get_full_path(),
+                    method=request.method,
+                    author=request.user.username
+                )
+            else:
+                # set default author value if user isn't authenticated
+                RequestKeeperModel.objects.create(
+                    path=request.get_full_path(),
+                    method=request.method
+                )
+
