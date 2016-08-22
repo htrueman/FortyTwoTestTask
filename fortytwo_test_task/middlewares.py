7074cda3c4485e2afe6559fef05df9d464445491
htrueman <vege1wgw@gmail.com>
2016-08-22 15:13:37 +0300

    middlware upd

--- fortytwo_test_task/middlewares.py
+++ fortytwo_test_task/middlewares.py
@@ -3,19 +3,17 @@ from apps.hello.models import RequestKeeperModel
 
 class RequestKeeperMiddleware(object):
     def process_response(self, request, response):
-        if not request.is_ajax():
-            if not request.path.startswith("/static/"):
-                if request.user.is_authenticated():
-                    req = RequestKeeperModel(
-                        method=request.method,
-                        name=request.path,
-                        status=response.status_code,
-                        author=request.user.username)
-                    req.save()
-                else:
-                    req = RequestKeeperModel(
-                        method=request.method,
-                        name=request.path,
-                        status=response.status_code)
-                    req.save()
+        if request.is_ajax() or request.path.startswith("/static/"):
+            return response
+
+        req = RequestKeeperModel(
+            method=request.method,
+            name=request.path,
+            status=response.status_code,
+            author=request.user.username
+        )
+        if request.user.is_authenticated():
+            req.author = request.user.username
+
+        req.save()
         return response
