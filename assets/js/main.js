a26ca25af76a90cc1be585ac99cb6f435af330aa
htrueman <vege1wgw@gmail.com>
2016-08-23 02:58:07 +0300

    t3: mw opd

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,7 @@ function updateRequests(data){
    last_id = data[0].pk;
    for(i=0;i<data.length;i++){
        var insert = '<div class="request">' +
+                      '<hr style="margin-left: -43% !important;" />' +
                       '<strong>' + data[i].fields.author + '  ' + '</strong>' +
                       '#' + data[i].pk + '  ' +
                       data[i].fields.date + '  ' +
