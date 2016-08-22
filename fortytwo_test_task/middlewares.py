65ddaa0ed76a84c9558b8086a1e8d51920507ac6
htrueman <vege1wgw@gmail.com>
2016-08-23 02:51:48 +0300

    t5: styles fixes

--- fortytwo_test_task/middlewares.py
+++ fortytwo_test_task/middlewares.py
@@ -3,19 +3,17 @@ from apps.hello.models import RequestKeeperModel
 
 class RequestKeeperMiddleware(object):
     def process_response(self, request, response):
-        if not request.is_ajax():
-            if not request.path.startswith("/static/"):
-                if request.user.is_authenticated():
-                    req = RequestKeeperModel(
-                        method=request.method,
-                        name=request.path,
-                        status=response.status_code,
-                        author=request.user.username)
-                    req.save()
-                else:
-                    req = RequestKeeperModel(
-                        method=request.method,
-                        name=request.path,
-                        status=response.status_code)
-                    req.save()
+        if request.is_ajax() or request.path.startswith("/static/"):
+            return response
+
+        req = RequestKeeperModel(
+            method=request.method,
+            name=request.path,
+            status=response.status_code,
+            author=request.user.username
+        )
+        if not request.user.is_authenticated():
+            req.author = RequestKeeperModel.\
+                            _meta.get_field('author').get_default()
+        req.save()
         return response
