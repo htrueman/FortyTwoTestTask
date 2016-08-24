824305226f7f800cbc80dd6ef7119433c99e6aad
htrueman <vege1wgw@gmail.com>
2016-08-24 12:30:41 +0300

    t3: middleware fix

--- fortytwo_test_task/middlewares.py
+++ fortytwo_test_task/middlewares.py
@@ -9,11 +9,10 @@ class RequestKeeperMiddleware(object):
         req = RequestKeeperModel(
             method=request.method,
             name=request.path,
-            status=response.status_code,
-            author=request.user.username
+            status=response.status_code
         )
-        if not request.user.is_authenticated():
-            req.author = RequestKeeperModel.\
-                            _meta.get_field('author').get_default()
+        if request.user.is_authenticated():
+            req.author = request.user.username
+
         req.save()
         return response
