b42739a49f923c4700937a8ca47a148e84b7f3d4
htrueman <vege1wgw@gmail.com>
2016-08-28 21:12:16 +0300

    t13: tests added

---
+++ apps/hello/tests/test_priority_change.py
@@ -0,0 +1,48 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from apps.hello.models import RequestKeeperModel
+
+
+class RequestsPriority(TestCase):
+    def test_ajax_post(self):
+        """ test for form ability to save data """
+        RequestKeeperModel.objects.create(
+            name='/test_path/',
+            status=200,
+            method='GET',
+        )
+        self.client.get(reverse('requests'))
+        self.client.post(reverse('requests'),
+                         data={'priority': 100,
+                               'pk': 1}, follow=True)
+        self.assertEqual(RequestKeeperModel.objects.first().priority, 100)
+
+    def test_ajax_possst(self):
+        """ test if form displaying errors with not valid data """
+        RequestKeeperModel.objects.create(
+            name='/test_path/',
+            status=200,
+            method='GET',
+        )
+        response = self.client.post(reverse('requests'),
+                                    data={'priority': 'bad',
+                                          'pk': 1}, follow=True)
+        self.assertFormError(response, 'req_form', 'priority',
+                             [u'Enter a whole number.'])
+
+    def test_ordering_by_priority(self):
+        """ test requests ordering by priority """
+        for i in range(20):
+            RequestKeeperModel.objects.create(
+                name='/' + str(i) + '/',
+                status=200,
+                method='GET',
+                priority=i
+            )
+        response = self.client.get(reverse('requests'))
+        RequestKeeperModel.objects.last().delete()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(list(RequestKeeperModel.objects.all().
+                              order_by('-priority', '-pk'))[:10],
+                         list(response.context['requests']))
