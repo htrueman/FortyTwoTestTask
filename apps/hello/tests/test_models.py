33508632f176db943d41107c9d87527c3544ee36
htrueman <vege1wgw@gmail.com>
2016-09-03 18:56:25 +0300

    t15: name validatation test

--- apps/hello/tests/test_models.py
+++ apps/hello/tests/test_models.py
@@ -26,10 +26,16 @@ class MyDataModelTests(TestCase):
                 skype='Skype',
                 other_conts='Conts'
             )
-        try:
+        with self.assertRaisesMessage(
+                                    ValidationError,
+                                    'Please, write your real date of birth!'):
             test.clean_fields()
-        except ValidationError:
-            self.assertIn("ValidationError", traceback.format_exc())
+        
+        with self.assertRaisesMessage(
+                                    ValidationError,
+                                    'This field should contain'
+                                    'alphabetic characters only!'):
+            test.clean_fields()
 
 
 class RequestKeeperModelTests(TestCase):
